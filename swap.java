3d9d37310e9199749b59e5449e15853354fdd694
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-07-28 08:29:52 -0700

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,13 @@
+class swap
+{
+public static void main(String sg[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+int a,b,c;
+a=b;
+b=c;
+c=a;
+system.out.print("Enter any numbers");
+system.out.print("swap=");
+}
+}
